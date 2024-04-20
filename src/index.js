7b7bd3fdf56f975461a6d63a9c65d53455ecb318
AliDev <julesalysa@gmail.com>
2024-04-20 22:53:11 +0800

    Authentication - 1 Milestone

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import SignupPage from './component/auth/SignupPage';
+import LoginPage from './component/auth/LoginPage';
+import FrontendPage from './component/landingpage/FrontendPage';
+import BackendPage from './component/landingpage/BackendPage';
+import reportWebVitals from './reportWebVitals';
+import{
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
+
+const router =createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+  },
+  {
+    path:'/signup',
+    element:<SignupPage/>,
+  },
+  {
+    path:'/login',
+    element:<LoginPage/>,
+  },
+  {
+    path:'/frontend',
+    element:<FrontendPage/>,
+  },
+  {
+    path:'/backend',
+    element:<BackendPage/>,
+  },
+])
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <RouterProvider router={router}/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
